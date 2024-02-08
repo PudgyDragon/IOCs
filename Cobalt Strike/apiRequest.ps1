1968c31d249d37760b16eca19c35d63b0bb1ac16
Pudgy Dragon <142341704+PudgyDragon@users.noreply.github.com>
2024-02-08 10:15:25 -0500

    Update apiRequest.ps1

--- Cobalt Strike/apiRequest.ps1
+++ Cobalt Strike/apiRequest.ps1
@@ -1,10 +1,16 @@
-# Here is a PowerShell script I used to get the SHA256 hash values from MalwareBazaar
+# PowerShell script for getting Cobalt Strike hash values from MalwareBazaar
 (Invoke-Webrequest -Method POST -Body "query=get_siginfo&signature=CobaltStrike&limit=1000" -Uri https://mb-api.abuse.ch/api/v1/).Content | Out-File -FilePath "C:\path\to\file.json"
 
 $json = (Get-Content "C:\path\to\file.json" -Raw) | ConvertFrom-Json
 $json | foreach {
     $_.data | foreach {
+        $md5 = $_.md5_hash 
+        Write-Output $md5 | Add-Content -Path "C:\path\to\md5.json"
+        $sha1 = $_.sha1_hash 
+        Write-Output $sha1 | Add-Content -Path "C:\path\to\sha1.json"
         $sha256 = $_.sha256_hash 
-        Write-Output $sha256 | Add-Content -Path "C:\path\to\newfile.json"
+        Write-Output $sha256 | Add-Content -Path "C:\path\to\sha256.json"
+        $sha384 = $_.sha3_384_hash 
+        Write-Output $sha384 | Add-Content -Path "C:\path\to\sha384.json"
     }
 }
