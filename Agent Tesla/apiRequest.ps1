abbb8166e2da22c4e13a84534d69709969e731e1
Pudgy Dragon <142341704+PudgyDragon@users.noreply.github.com>
2024-06-06 06:52:17 -0400

    Update apiRequest.ps1
    Added if/else statements for ThreatFox

--- Agent Tesla/apiRequest.ps1
+++ Agent Tesla/apiRequest.ps1
@@ -15,3 +15,42 @@ $json | foreach {
         Write-Output $sha384 | Add-Content -Path "C:\path\to\sha384.json"
     }
 }
+
+# Powershell script for getting Agent Tesla IOCs from ThreatFox
+(Invoke-Webrequest -Method POST -Body '{ "query": "malwareinfo", "malware": "AgentTesla", "limit": 1000 }' -Uri https://threatfox-api.abuse.ch/api/v1/).Content | Out-File -FilePath "C:\path\to\file.json"
+
+$json = (Get-Content "C:\path\to\file.json" -Raw) | ConvertFrom-Json
+$json | foreach {
+    $_.data | foreach {
+        $ioc = $_.ioc
+        $type = $_.ioc_type
+        if ($type -eq "domain")
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc_domain.json"
+        }    
+        elseif ($type -eq "ip:port")
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc_ip.json"
+        }
+        elseif ($type -eq "url")
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc_url.json"
+        }
+        elseif ($type -eq "md5")
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc_md5.json"
+        }
+        elseif ($type -eq "sha1")
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc_sha1.json"
+        }
+        elseif ($type -eq "sha256")
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc_sha256.json"
+        }
+        else
+        {
+            Write-Output $ioc | Add-Content -Path "C:\path\to\ioc.json"
+        }
+    }
+}
